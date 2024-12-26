7dfcb7a0fd8bf9eb36188a8712bdec0eb84c9d77
NNyda <dannyadjenia@gmail.com>
2024-12-26 03:30:49 +0100

    Ponctuation à la phrase du fichier cadavre_exquis.md

---
+++ script1.R
@@ -0,0 +1,11 @@
+# Création du fichier "votre_idep1.md", celui de Nnyda
+file.create ("votre_idep1.md")
+
+# Edition de la première ligne de mon fichier "votre_idep.md".
+writeLines ("Bonjour, ceci est la première ligne de Nnyda", "votre_idep.md")
+
+#Edition de la deuxième ligne de mon fichier "votre_idep.md".
+writeLines ("Bonjour, ceci est ma deuxième ligne", "votre_idep.md", "append=TRUE")
+
+#Un peu de piment dans le fichier "votre_idep.md" de mon poto Ayefemi
+writeLines ("Je viens pas la présente mettre un peu de piement dans le fichier de mon poto", "votre_idep.md", "append=TRUE")
